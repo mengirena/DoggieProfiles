64a9c4fdbc5d4baf880fa2c9c885ec3e88d25c6e
Irena Chen <irena1009@gmail.com>
2021-07-17 22:51:37 -0700

    add next button

--- Card.js
+++ Card.js
@@ -44,6 +44,7 @@ class Card extends React.Component{
       ]
     } 
     this.handleClickPrev = this.handleClickPrev.bind(this)
+    this.handleClickNext = this.handleClickNext.bind(this)
   }
 
   
@@ -61,7 +62,7 @@ class Card extends React.Component{
                     dogObj.dogName = data[0].results[0].name.first
                     dogObj.dogImg = data[1].message
                     this.setState(preState => {
-                      dogObj.dogId = preState.dogs[0].dogId -1 
+                      dogObj.dogId = preState.dogs[0].dogId - 1 
                       let keep = preState.dogs.filter((dog,i) =>{
                         if (i!== 2) return true
                         return false
@@ -72,21 +73,48 @@ class Card extends React.Component{
                 }))
 
   }
+
+  handleClickNext(){
+    console.log("clicked")
+
+    let dogObj = {}
+
+    Promise.all([fetch("https://randomuser.me/api/"),fetch("https://dog.ceo/api/breeds/image/random")])
+    // Promise.all([fetch(ranName),fetch(dogImg)])
+    .then(res => Promise.all(res.map(res=>res.json()))
+                .then(data=>{
+                    console.log("ranName",data[0].results[0].name.first)
+                    console.log("dogImg", data[1].message)
+                    dogObj.dogName = data[0].results[0].name.first
+                    dogObj.dogImg = data[1].message
+                    this.setState(preState => {
+                      dogObj.dogId = preState.dogs[2].dogId + 1 
+                      let keep = preState.dogs.filter((dog,i) =>{
+                        if (i!== 0) return true
+                        return false
+                      } )
+                      return {dogs:[...keep, dogObj]}
+                    })
+                    console.log(dogObj)
+                }))
+
+  }
+
+  
   
   render(){
     let result = []
     for (let i = 0 ; i < 3 ; i++ ){
-      result.push( <div className="profile" key={this.state.dogs[i].dogId}><img src={this.state.dogs[i].dogImg} alt="" className="pic"/><p>{this.state.dogs[i].dogName}</p></div>)
+      result.push( <div className="profile" key={this.state.dogs[i].dogId} ><img src={this.state.dogs[i].dogImg} alt="" className="pic"/><p>{this.state.dogs[i].dogName}</p></div>)
     }
+    leftBtn.addEventListener("click",this.handleClickPrev) 
+    rightBtn.addEventListener("click",this.handleClickNext) 
     
     console.log(result)
     return(
       <>
         {result}
-        <button id="left" onClick={this.handleClickPrev}>👈🏼</button>
-        <button id="right">👉🏼</button>
       </>
-      
   
     )
   }
@@ -94,4 +122,6 @@ class Card extends React.Component{
 }
 
 const domContainer = document.querySelector('#cat-basket');
+const leftBtn = document.querySelector('#left')
+const rightBtn = document.querySelector('#right')
 ReactDOM.render(<Card />, domContainer);
